56df8a7b296001f5a81297907e4e4be0b60a344b
maanasags <maanasa@foxsense.io>
2024-03-09 23:32:19 +0530

    repo set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "@fontsource/source-sans-pro"; 
+import "@fontsource/source-sans-pro/400.css"; 
+import "@fontsource/source-sans-pro/600.css"; 
+import "@fontsource/source-sans-pro/700.css"; 
+import "@fontsource/source-code-pro"; 
+import "@fontsource/source-code-pro/400.css"; 
+import "@fontsource/source-code-pro/600.css"; 
+import "@fontsource/source-code-pro/700.css"; 
+import CustomLayout from './pages/layout';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CustomLayout/>
     </div>
   );
 }
---
+++ src/pages/layout.tsx
@@ -0,0 +1,10 @@
+import './layout.css'
+import React from 'react'
+
+export default function CustomLayout() {
+  return (
+    <div id='gradientContainer'>
+      dlzuifgds
+    </div>
+  )
+}
